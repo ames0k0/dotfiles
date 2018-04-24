ffdf996755847d22d780a0524010ba51b2127d18
kira <nodirjon128.ng@gmail.com>
2018-04-24 18:26:36 +0300

    run all arguments

--- install.sh
+++ install.sh
@@ -2,27 +2,25 @@
 
 # https://www.youtube.com/watch?v=5r6yzFEXajQ&t=2220s
 
-if [ $1 == '-ti' ]; then
-    git clone https://github.com/gpakosz/.tmux.git
-    ln -s -f .tmux/.tmux.conf
-    cp .tmux/.tmux.conf.local .
-
-elif [ $1 == '-tc' ]; then
-    cp -f .tmux.conf ~/
-    cp -f .tmux.conf.local ~/
-
-elif [ $1 == '-vi' ]; then
-    wget -qO- https://raw.github.com/ma6174/vim/master/setup.sh | sh -x
-
-elif [ $1 == '-vc' ]; then
-    cp -f .vimrc ~/
-
-elif [ $1 == '-pi' ]; then
-    sudo -H pip3 install pygame Cython keras scikit-learn tensorflow matplotlib numpy pandas \
-                         seaborn Tkinter virtualenv flask flask-admin flask-sqlalchemy django \
-                         requests bs4 img2pdf pdir2 selenium pydub nltk gensim
-fi
-
+for i in $@
+do
+    if [ $i == '-ti' ]; then
+        git clone https://github.com/gpakosz/.tmux.git
+        ln -s -f .tmux/.tmux.conf
+        cp .tmux/.tmux.conf.local .
+    elif [ $i == '-tc' ]; then
+        cp -f .tmux.conf ~/
+        cp -f .tmux.conf.local ~/
+    elif [ $i == '-vi' ]; then
+        wget -qO- https://raw.github.com/ma6174/vim/master/setup.sh | sh -x
+    elif [ $i == '-vc' ]; then
+        cp -f .vimrc ~/
+    elif [ $i == '-pi' ]; then
+        sudo -H pip3 install pygame Cython keras scikit-learn tensorflow matplotlib numpy pandas \
+                             seaborn Tkinter virtualenv flask flask-admin flask-sqlalchemy django \
+                             requests bs4 img2pdf pdir2 selenium pydub nltk gensim
+    fi
+done
 ## bashrc --
 # PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\][kira@-築城院 真鍳]\[\033[34m\]:\[\033[01;95m\]\W\[\033[00m\]\n\$ '
 
